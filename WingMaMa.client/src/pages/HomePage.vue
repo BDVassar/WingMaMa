7085c67336ba25b57c1b4d579f8b6e2e57fe2333
LiamSmith1992 <liamj.smith@hotmail.com>
2023-02-27 15:05:16 -0700

    some general looking stuff

--- WingMaMa.client/src/pages/HomePage.vue
+++ WingMaMa.client/src/pages/HomePage.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid bg-color">
+    <section class="row">
+      <div class="col-12 text-center">
+        <h1>WingMaMa</h1>
+      </div>
+    </section>
+    <section class="row">
+
+      <div class="col-4">
+        <button class="btn btn-warning mdi mdi-baby-bottle">Feeding</button>
+      </div>
+      <div class="col-4">
+        <button class="btn btn-warning mdi mdi-human-baby-changing-table"> Diaper </button>
+      </div>
+      <div class="col-4">
+        <button class="btn btn-warning mdi mdi-sleep">Nap</button>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -22,23 +30,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.bg-color {
+  background-color: rgb(24, 159, 208);
+  height: 100vh;
 }
 </style>
