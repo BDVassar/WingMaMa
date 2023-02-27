ff8f7c06f033babdb99e5eabe778ca219ddb51b1
LiamSmith1992 <liamj.smith@hotmail.com>
2023-02-27 15:25:23 -0700

    started feeding back-end but ran out of time

---
+++ WingMaMa/Controllers/FeedingController.cs
@@ -0,0 +1,34 @@
+
+
+namespace WingMaMa.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FeedingController : ControllerBase
+{
+  private readonly Auth0Provider _auth;
+  private readonly FeedingService _FeedingService;
+
+  public FeedingController(Auth0Provider auth, FeedingService feedingService)
+  {
+    _auth = auth;
+    _FeedingService = feedingService;
+  }
+
+  [HttpPost]
+  [Authorize]
+  public async Task<ActionResult<Feeding>> Create([FromBody] Feeding FeedData)
+  {
+    try
+    {
+      Account userInfo = await _auth.GetUserInfoAsync<Account>(HttpContext);
+      Feeding Feed = _FeedingService.Create(FeedData);
+      return Ok(Feed);
+    }
+    catch (System.Exception)
+    {
+
+      throw;
+    }
+  }
+}
---
+++ WingMaMa/Services/FeedingService.cs
@@ -0,0 +1,16 @@
+
+
+namespace WingMaMa.Services;
+
+public class FeedingService
+{
+  private readonly FeedingRepository _repo;
+
+  public FeedingService(FeedingRepository repo)
+  {
+    _repo = repo;
+  }
+
+
+
+}
